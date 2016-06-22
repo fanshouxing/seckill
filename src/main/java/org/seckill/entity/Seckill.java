2b251938c89e522d123efe5a7b0ff2491576add1
范守星 <fanshouxing@quyiyuan.com>
2016-06-22 18:36:11 +0800

    Test

--- src/main/java/org/seckill/entity/Seckill.java
+++ src/main/java/org/seckill/entity/Seckill.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2016. 上海趣医网络技术服务有限公司 版权所有
- * Shanghai QuYi Network Technology Co., Ltd. All Rights Reserved.
- *
- * This is NOT a freeware,use is subject to license terms.
- */
 
 package org.seckill.entity;
 
